dac4151be96939ad2eaac6ba92730faca4251ed2
jbrognoli <brognolijoao@gmail.com>
2024-05-05 23:07:15 -0300

    fix: commits

--- javascript.js
+++ javascript.js
@@ -4,6 +4,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const dogImg = document.querySelector(".mainContainerImg");
     const pawImg = document.querySelector(".mainContainerImgPaw");
     const catImg = document.querySelector(".mainContainerImgCat");
+    const faqLeftPaw = document.querySelector(".mainContainerFaqImgPaws");
+    const faqRightPaw = document.querySelector(".mainContainerFaqImgPaws2");
   
     buttons.forEach(function (button) {
       button.addEventListener("click", function () {
@@ -32,18 +34,26 @@ document.addEventListener("DOMContentLoaded", function () {
           dogImg.style.display = "block";
           pawImg.style.display = "block";
           catImg.style.display = "block";
-        } else if (section === "services") {
-          dogImg.style.display = "none";
-          pawImg.style.display = "none";
-          catImg.style.display = "none";
+          faqLeftPaw.style.display = "none";
+          faqRightPaw.style.display = "none";
         } else if (section === "helper") {
           dogImg.style.display = "none";
           pawImg.style.display = "none";
           catImg.style.display = "none";
+          faqLeftPaw.style.display = "none";
+          faqRightPaw.style.display = "none";
+        } else if (section === "donation") {
+          dogImg.style.display = "none";
+          pawImg.style.display = "none";
+          catImg.style.display = "none";
+          faqLeftPaw.style.display = "block";
+          faqRightPaw.style.display = "block";
         } else if (section === "faq") {
           dogImg.style.display = "none";
           pawImg.style.display = "none"; // Adjust as needed
           catImg.style.display = "none"; // Adjust as needed
+          faqLeftPaw.style.display = "block";
+          faqRightPaw.style.display = "block";
         }
       });
     });
